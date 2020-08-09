b0b56459a2de34d876b7aadc9cf8108a302fba54
Wilfried <wilfried.djopa@gmail.com>
2020-08-09 22:09:14 +0200

    Update app component'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { SeoService } from './services/seo.service';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
+
+let deferredPrompt;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,73 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerce-angular-template';
+
+  constructor(
+    private route: ActivatedRoute, private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private seoService: SeoService
+  ) {
+
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI to notify the user they can add to home screen
+      // addBtn.style.display = 'block';
+      console.log(e)
+      // addBtn.addEventListener('click', (e) => {
+      //   // hide our user interface that shows our A2HS button
+      //   addBtn.style.display = 'none';
+      //   // Show the prompt
+      //   deferredPrompt.prompt();
+      //   // Wait for the user to respond to the prompt
+      //   deferredPrompt.userChoice.then((choiceResult) => {
+      //     if (choiceResult.outcome === 'accepted') {
+      //       console.log('User accepted the A2HS prompt');
+      //     } else {
+      //       console.log('User dismissed the A2HS prompt');
+      //     }
+      //     deferredPrompt = null;
+      //   });
+      // });
+    });
+
+  }
+
+  ngOnInit() {
+
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map(() => this.route),
+      map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      filter((route) => route.outlet === 'primary'),
+      mergeMap((route) => route.data)
+    )
+      .subscribe((event) => {
+        this.seoService.updateTitle(event['title']);
+        this.seoService.updateOgUrl(event['ogUrl']);
+        this.seoService.updateOgImage(event['ogImage']);
+        //Updating Description tag dynamically with title
+        this.seoService.updateDescription(event['title'] + event['description'])
+      });
+  }
+
+  onActivate(event: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      let scrollToTop = window.setInterval(() => {
+        let pos = window.pageYOffset;
+        if (pos > 0) {
+          window.scrollTo(0, 0);
+          // window.scrollTo(0, pos-50); // animation how far to scroll on each step
+        } else {
+          window.clearInterval(scrollToTop);
+        }
+      }, 0);
+    }
+  }
 }
