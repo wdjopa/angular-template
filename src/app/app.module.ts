6cf77213375ba0d512c3cd9aaa00d72b7ab3f3eb
Wilfried <wilfried.djopa@gmail.com>
2020-08-09 20:46:07 +0200

    Ajout de firebase et angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { Routes, RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -19,7 +25,79 @@ import { ReviewComponent } from './modal/review/review.component';
 import { LoaderComponent } from './modal/loader/loader.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppPasswordDirective } from './directives/app-password.directive';
+
+import { DataService } from './services/data.service';
+import { AuthGuard } from './guard/auth.guard';
+import { CartGuard } from './guard/cart.guard';
+import { UserService } from './services/user.service';
+import { NavigationService } from './services/navigation.service';
+import { SeoService } from './services/seo.service';
+
+
+
+const appRoutes: Routes = [
+  {
+    path: "", component: HomeComponent, data: {
+      title: 'Accueil',
+      description: 'Osez commander chez La Mater Express - Plus de 1000 plats et menus livrés chez vous | La Mater Express',
+      ogImage: 'https://express.lamaterservice.com/assets/img/banner/banner-3.jpg'
+    }
+  },
+  {
+    path: "accueil", component: HomeComponent, data: {
+      title: 'Accueil',
+      description: 'Osez commander chez La Mater Express - Plus de 1000 plats et menus livrés chez vous | La Mater Express',
+      ogImage: 'https://express.lamaterservice.com/assets/img/banner/banner-3.jpg'
+    }
+  },
+  { path: "deconnexion", canActivate: [AuthGuard], component: LogoutComponent },
+  {
+    path: "connexion", component: LoginComponent, data: {
+      title: 'Connexion',
+      description: 'Connectez-vous et commandez parmi plus de 1000 plats et menus livrés chez vous | La Mater Express',
+      ogImage: 'https://express.lamaterservice.com/assets/img/banner/banner-3.jpg'
+    }
+  },
+  {
+    path: "inscription", component: RegisterComponent, data: {
+      title: 'Inscription',
+      description: 'Inscrivez-vous et commandez parmi plus de 1000 plats et menus livrés chez vous | La Mater Express',
+      ogImage: 'https://express.lamaterservice.com/assets/img/banner/banner-3.jpg'
+    }
+  },
+  {
+    path: "collections", component: CollectionsComponent, data: {
+      title: 'Nos collections',
+      description: 'Découvrez et commandez parmi plus de 1000 plats et menus livrés chez vous | La Mater Express',
+      ogImage: 'https://express.lamaterservice.com/assets/img/banner/banner-3.jpg'
+    }
+  },
+  {
+    path: "collection/:collection", component: CollectionComponent, data: {
+      title: 'Collection',
+      description: 'Découvrez et commandez parmi plus de 1000 plats et menus livrés chez vous | La Mater Express',
+      ogImage: 'https://express.lamaterservice.com/assets/img/banner/banner-3.jpg'
+    }
+  },
+  {
+    path: "produits/:produit", component: ProduitComponent, data: {
+      title: 'Produit detail',
+      description: 'Découvrez et commandez parmi plus de 1000 plats et menus livrés chez vous | La Mater Express',
+      ogImage: 'https://express.lamaterservice.com/assets/img/banner/banner-3.jpg'
+    }
+  },
+  { path: "panier", component: CartComponent },
+  { path: "caisse", canActivate: [CartGuard, AuthGuard], component: CheckoutComponent },
+  { path: "compte/:slug", canActivate: [AuthGuard], component: AccountComponent },
+  { path: "compte", canActivate: [AuthGuard], component: AccountComponent },
+  { path: "**", component: FourOhFourComponent },
+];
+
+
 
 @NgModule({
   declarations: [
@@ -42,11 +120,27 @@ import { AppPasswordDirective } from './directives/app-password.directive';
     FooterComponent,
     AppPasswordDirective
   ],
+
+  entryComponents: [
+    LoaderComponent,
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    DataService,
+    SeoService,
+    UserService,
+    NavigationService,
+    AuthGuard,
+    CartGuard,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
