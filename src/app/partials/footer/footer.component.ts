bced7026b00d757bdc2073ada8a9a4ddc5333543
Wilfried Djopa <wilfried.djopa@gmail.com>
2022-03-21 10:34:16 +0100

    get version from footer

--- src/app/partials/footer/footer.component.ts
+++ src/app/partials/footer/footer.component.ts
@@ -2,34 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ConfigService } from 'src/app/services/config.service';
 import { NavigationService } from 'src/app/services/navigation.service';
-
+import { version } from '../../../../package.json';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-
-  version = "1.1.4"
+  version = version;
   company: any;
   companySubscription: Subscription;
   storage_url: string;
 
-
-  constructor(private navigationService: NavigationService, private configService: ConfigService) {
+  constructor(
+    private navigationService: NavigationService,
+    private configService: ConfigService
+  ) {
     this.storage_url = this.configService.storage;
   }
 
   ngOnDestroy() {
-    this.companySubscription.unsubscribe()
+    this.companySubscription.unsubscribe();
   }
 
   ngOnInit(): void {
-    this.companySubscription = this.navigationService.companySubject.subscribe(company => {
-      if (company) {
-        this.company = company;
+    this.companySubscription = this.navigationService.companySubject.subscribe(
+      (company) => {
+        if (company) {
+          this.company = company;
+        }
       }
-    });
+    );
     this.navigationService.emitCompany();
   }
 }
